2a0075e2e1a4f206b4043dd3d13faf9c1bd48881
caloy0714 <58654567+caloy0714@users.noreply.github.com>
2023-06-14 14:09:06 +0800

    Update and rename AddEmployee.xaml.cs to AddAnimal.xaml.cs

--- App1/App1/View/AddAnimal.xaml.cs
+++ App1/App1/View/AddAnimal.xaml.cs
@@ -12,7 +12,7 @@ using Xamarin.Forms.Xaml;
 namespace App1.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AddEmployee : ContentPage
+    public partial class AddAnimal : ContentPage
     {
         EmployeeViewModel _viewModel;
         bool _isUpdate;
@@ -20,13 +20,13 @@ namespace App1.View
         public AddEmployee()
         {
             InitializeComponent();
-            _viewModel = new EmployeeViewModel();
+            _viewModel = new AnimalViewModel();
             _isUpdate = false;
         }
         public AddEmployee(EmployeeModel obj)
         {
             InitializeComponent();
-            _viewModel = new EmployeeViewModel();
+            _viewModel = new AnimalViewModel();
             if (obj != null)
             {
                 employeeID = obj.Id;
@@ -41,7 +41,7 @@ namespace App1.View
 
         private async void btnSaveUpdate_Clicked(object sender, EventArgs e)
         {
-            EmployeeModel obj = new EmployeeModel();
+            AnimalModel obj = new AnimalModel();
             obj.Animal = txtAnimal.Text;
             obj.Characteristics = txtChar.Text;
             obj.Species = txtSpecies.Text;
@@ -50,7 +50,7 @@ namespace App1.View
             if (_isUpdate)
             {
                 obj.Id = employeeID;
-                await _viewModel.UpdateEmployee(obj);
+                await _viewModel.UpdateAnimal(obj);
             }
             else
             {
